00f0f41034d039e0e83b68804d479706dbd814a3
Dudzpedra <poliveiraeduardo@gmail.com>
2022-04-22 15:03:49 -0300

    fix: sortService

--- src/services/sortService.ts
+++ src/services/sortService.ts
@@ -2,10 +2,10 @@ import { Article } from "../utils/types";
 
 export const sortByNewest = (list: Article[]) =>
   [...list].sort((a, b) =>
-    new Date(a.publishedAt).getTime() > new Date(b.title).getTime() ? -1 : 1
+    new Date(a.publishedAt).getTime() > new Date(b.publishedAt).getTime() ? -1 : 1
   );
 
   export const sortByOldest = (list: Article[]) =>
   [...list].sort((a, b) =>
-    new Date(a.publishedAt).getTime() > new Date(b.title).getTime() ? 1 : -1
+    new Date(a.publishedAt).getTime() > new Date(b.publishedAt).getTime() ? 1 : -1
   );
