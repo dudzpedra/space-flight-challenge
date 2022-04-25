e56c4751832254ef04018d434a7fd53f4954f17b
Dudzpedra <poliveiraeduardo@gmail.com>
2022-04-25 11:25:15 -0300

    fix: deleted comments

--- src/components/ArticleDetails/ArticleDetails.test.tsx
+++ src/components/ArticleDetails/ArticleDetails.test.tsx
@@ -1,20 +1,6 @@
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
-//import userEvent from '@testing-library/user-event'
 import ArticleDetails from '.'
-
-/* test('clicking the article details button opens the modal', async () => {
-  const mockHandler = jest.fn()
-
-  const user = userEvent
-  
-  render(<ArticleDetails />)
-
-  const button = screen.getByTestId('article-btn')
-  await user.click(button)
-  
-  expect(mockHandler.mock.calls).toHaveLength(1)
-}) */
 
 test('renders button', () => {
   const handleOpen = () => console.log('modal open')
